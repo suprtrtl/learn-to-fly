f6c994d74b7fabaec5e4f3a8179f8aee04d5977d
suprtrtl <owen.rodway@gmail.com>
2025-07-19 20:23:42 -0600

    npm fix

--- target/debug/liblib_neural_network.d
+++ target/debug/liblib_neural_network.d
@@ -1,1 +1,1 @@
-C:\Users\owenr\dev\rust\learn-to-fly\target\debug\liblib_neural_network.rlib: C:\Users\owenr\dev\rust\learn-to-fly\libs\neural-network\src\lib.rs
+/home/suprtrtl/dev/learn-to-fly/target/debug/liblib_neural_network.rlib: /home/suprtrtl/dev/learn-to-fly/libs/neural-network/src/lib.rs
